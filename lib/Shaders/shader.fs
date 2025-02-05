deefbf22a56b97660fe247df9e777a9a99b7db8c
BurakOrtakuz <burakortakuz@gmail.com>
2025-02-05 14:00:02 +0300

    Lib

---
+++ lib/Shaders/shader.fs
@@ -0,0 +1,12 @@
+#version 330 core
+out vec4 FragColor;
+
+in vec3 ourColor;
+in vec2 TexCoord;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+void main()
+{
+    vec2 reversedTexCoord = vec2(1.0- TexCoord.x, 1.0 - TexCoord.y);
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, reversedTexCoord), 0.2);
+}
