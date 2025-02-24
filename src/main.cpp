4a934339a73db9fea7d574e78edde877d23425dd
burakortakuz <burakortakuz@gmail.com>
2025-02-24 19:51:43 +0300

    Directional Light nothing much interesting

--- src/main.cpp
+++ src/main.cpp
@@ -119,6 +119,18 @@ int main()
 	glEnableVertexAttribArray(0);
 
 
+	glm::vec3 cubePositions[] = {
+        glm::vec3( 0.0f,  0.0f,  0.0f),
+        glm::vec3( 2.0f,  5.0f, -15.0f),
+        glm::vec3(-1.5f, -2.2f, -2.5f),
+        glm::vec3(-3.8f, -2.0f, -12.3f),
+        glm::vec3( 2.4f, -0.4f, -3.5f),
+        glm::vec3(-1.7f,  3.0f, -7.5f),
+        glm::vec3( 1.3f, -2.0f, -2.5f),
+        glm::vec3( 1.5f,  2.0f, -2.5f),
+        glm::vec3( 1.5f,  0.2f, -1.5f),
+        glm::vec3(-1.3f,  1.0f, -1.5f)
+    };
 	//Texture
 
 	unsigned int diffuseMap = loadTexture ("./assets/container2.png");
@@ -147,6 +159,7 @@ int main()
         // be sure to activate shader when setting uniforms/drawing objects
         lightShader.use();
         lightShader.setVec3("light.position", lightPos);
+		lightShader.setVec3("light.direction", -0.2f, -1.0f, -0.3f);
         lightShader.setVec3("viewPos", camera.getPosition());
 
         // light properties
@@ -177,7 +190,17 @@ int main()
 
         // render the cube
         glBindVertexArray(cubeVAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36);
+        for (unsigned int i = 0; i < 10; i++)
+        {
+            // calculate the model matrix for each object and pass it to shader before drawing
+            glm::mat4 model = glm::mat4(1.0f);
+            model = glm::translate(model, cubePositions[i]);
+            float angle = 20.0f * i;
+            model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            lightShader.setMat4("model", model);
+
+            glDrawArrays(GL_TRIANGLES, 0, 36);
+        }
 
 
         // also draw the lamp object
